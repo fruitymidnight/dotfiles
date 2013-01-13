734b5761f26d7d4451fb82d9535a839d91d178b5
Shin <shin@fruitymidnight.net>
2013-01-13 17:27:19 +0900

    Modify anything setting.   M-x calls anything-M-x   M-y calls anything-show-kill-ring

--- emacs/.emacs.d/config/packages/init-anything.el
+++ emacs/.emacs.d/config/packages/init-anything.el
@@ -3,7 +3,9 @@
 (unless (locate-library "anything")
   (el-get 'sync 'anything))
 
-(define-key global-map  (kbd "C-l") 'anything)
+(require 'anything-startup)
+(define-key global-map (kbd "C-l") 'anything)
 (define-key global-map (kbd "C-x b") 'anything-for-files)
 (define-key global-map (kbd "M-y") 'anything-show-kill-ring)
+(define-key global-map (kbd "M-x") 'anything-M-x)
 
