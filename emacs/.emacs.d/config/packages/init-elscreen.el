a6b3808da79f82fcbc07a49b3d8d763dedfb479b
Shin <shin@fruitymidnight.net>
2013-01-13 20:43:26 +0900

    Add setting for elscreen.

--- emacs/.emacs.d/config/packages/init-elscreen.el
+++ emacs/.emacs.d/config/packages/init-elscreen.el
@@ -9,8 +9,23 @@
 (setq elscreen-tab-display-kill-screen nil) ; タブの左端の×を非表示
 (load "elscreen" "ElScreen" t)
 
-;; (require 'elscreen-gf)
-;; (require 'elscreen-dired)
-;; (require 'elscreen-color-theme)
-;; (require 'elscreen-howm)
-;; (require 'elscreen-server)
+(unless (locate-library "elscreen-gf")
+  (el-get 'sync 'elscreen-gf))
+(require 'elscreen-gf)
+
+(unless (locate-library "elscreen-dired")
+  (el-get 'sync 'elscreen-dired))
+(require 'elscreen-dired)
+
+(unless (locate-library "elscreen-color-theme")
+  (el-get 'sync 'elscreen-color-theme))
+(require 'elscreen-color-theme)
+
+(unless (locate-library "elscreen-server")
+  (el-get 'sync 'elscreen-server))
+(require 'elscreen-server)
+
+(unless (locate-library "elscreen-dnd")
+  (el-get 'sync 'elscreen-dnd))
+(require 'elscreen-dnd)
+
