37b7c9c76954bd6a5c5fefc796331a43394e2f53
Shin <shin@fruitymidnight.net>
2013-01-13 16:28:42 +0900

    Modify anythng setting.

--- emacs/.emacs.d/config/packages/init-anything.el
+++ emacs/.emacs.d/config/packages/init-anything.el
@@ -4,4 +4,6 @@
   (el-get 'sync 'anything))
 
 (define-key global-map  (kbd "C-l") 'anything)
+(define-key global-map (kbd "C-x b") 'anything-for-files)
+(define-key global-map (kbd "M-y") 'anything-show-kill-ring)
 
