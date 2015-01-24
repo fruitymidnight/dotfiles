2be9ae790068026c02243420985ef4ce216deb09
Shin <shin@fruitymidnight.net>
2015-01-25 00:54:28 +0900

    Add snippet setting.

--- emacs/.emacs.d/config/packages/init-yasnippet.el
+++ emacs/.emacs.d/config/packages/init-yasnippet.el
@@ -1,6 +1,20 @@
-; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
-;;------------------------------------------------------------------------------
-;; @ yasnippet
+;;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;;; init-yasnippet.el --- 
+
+;;; Commentary:
+
+;; snippet の作成方法は下記頁を参考にする
+;; @see http://yasnippet-doc-jp.googlecode.com/svn/trunk/doc-jp/snippet-development.html
+
+;;; Code:
+
+
+;;; -------------------------------------------------------------------
+;;;; Yasnippet
+;;; -------------------------------------------------------------------
+
+
+
 
 (when (locate-library "helm-c-yasnippet")
   (require 'helm-c-yasnippet)
@@ -10,8 +24,8 @@
 (lazyload (helm-c-yas-complete) "yasnippet"          
           (setq yas-snippet-dirs
                 '(
-                  "~/.emacs.d/var/snippets"                         ; personal snippets
-                  "~/.emacs.d/elpa/oq/snippets" ; default collection
+                  "~/.emacs.d/var/snippets"                        ; personal snippets
+                  yas-installed-snippets-dir                       ; default collection
                   ))
           (yas-global-mode 1)
           )
